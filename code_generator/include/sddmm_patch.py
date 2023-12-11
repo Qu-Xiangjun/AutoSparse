e680c42c34f5d3c687ac4ccbbda9b533b6a8f90a
quxiangjun <quxiangjun@mail.ustc.edu.cn>
2023-12-11 18:59:37 +0800

    init & Add notation

--- code_generator/include/sddmm_patch.py
+++ code_generator/include/sddmm_patch.py
@@ -2,13 +2,15 @@ import sys
 import re
 
 if __name__ == "__main__":
-    f = open(sys.argv[1])
+    f = open(sys.argv[1]) # ./taco_kernel.c
     s = f.read()
     f.close()
 
     f = open(sys.argv[1], "w")
-    apos = re.findall("A_vals\[(.*?)\]", s)[0]
-    substitute_C_vals = re.sub("C_vals\[(.*?)\]", 'C_vals[{}]'.format(apos), s)
+    apos = re.findall("A_vals\[(.*?)\]", s)[0] # 找到 A_vals[] 括号中的内容
+    # 字符串 C_vals[] 中括号中的内容替换为之前获取的 apos 变量的值c
+    substitute_C_vals = re.sub("C_vals\[(.*?)\]", 'C_vals[{}]'.format(apos), s) 
+    # 将形如 C_vals[] = 0.0; 的内容用空字符串替换，即注释掉这样的行。
     commentout_zero = re.sub("C_vals\[.*\] = 0.0;", "", substitute_C_vals)
     f.write(commentout_zero)
     f.close()
--- code_generator/include/time.hpp
+++ code_generator/include/time.hpp
@@ -3,22 +3,33 @@
 
 #include <chrono>
 using namespace std;
-using Clock=::chrono::high_resolution_clock;
+using Clock = ::chrono::high_resolution_clock;
 
-float inline compute_clock_micro(chrono::steady_clock::time_point t2, chrono::steady_clock::time_point t1) {
-  return (double)(chrono::duration_cast<chrono::microseconds>(t2 - t1).count());
+/**
+ * 两个时间点之间的微秒时间差
+ * 输入可以是 <chrono> 库中的 time_point 和 duration 类型
+ */
+float inline compute_clock_micro(chrono::steady_clock::time_point t2, chrono::steady_clock::time_point t1)
+{
+	return (double)(chrono::duration_cast<chrono::microseconds>(t2 - t1).count());
 }
 
-float inline compute_clock_micro(chrono::system_clock::time_point t2, chrono::system_clock::time_point t1) {
-  return (double)(chrono::duration_cast<chrono::microseconds>(t2 - t1).count());
+float inline compute_clock_micro(chrono::system_clock::time_point t2, chrono::system_clock::time_point t1)
+{
+	return (double)(chrono::duration_cast<chrono::microseconds>(t2 - t1).count());
 }
 
-double inline compute_clock(chrono::steady_clock::time_point t2, chrono::steady_clock::time_point t1) {
-  return (double)(chrono::duration_cast<chrono::nanoseconds>(t2 - t1).count()) / 1000000.0;
+/**
+ * 两个时间点之间的毫秒时间差
+ */
+double inline compute_clock(chrono::steady_clock::time_point t2, chrono::steady_clock::time_point t1)
+{
+	return (double)(chrono::duration_cast<chrono::nanoseconds>(t2 - t1).count()) / 1000000.0;
 }
 
-double inline compute_clock(chrono::system_clock::time_point t2, chrono::system_clock::time_point t1) {
-  return (double)(chrono::duration_cast<chrono::nanoseconds>(t2 - t1).count()) / 1000000.0;
+double inline compute_clock(chrono::system_clock::time_point t2, chrono::system_clock::time_point t1)
+{
+	return (double)(chrono::duration_cast<chrono::nanoseconds>(t2 - t1).count()) / 1000000.0;
 }
 
-#endif 
+#endif
