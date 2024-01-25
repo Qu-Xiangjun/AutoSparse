8e6ca1f5c1c3a157d7da43a7dd4263ae827d9ef9
quxiangjun <quxiangjun@mail.ustc.edu.cn>
2024-01-25 22:56:37 +0800

    Get and analysis dataset from suitsparse

---
+++ dataset/dataset_analyse.py
@@ -0,0 +1,69 @@
+import os, sys
+import numpy as np
+import csv
+
+dataset_dir = os.path.dirname(os.path.abspath(__file__))
+dataset_dir = os.path.dirname(dataset_dir)
+dataset_dir = os.path.join(dataset_dir, 'pretrained', 'dataset')
+
+def get_all_files_in_directory(directory):
+    file_names = []
+    for filename in os.listdir(directory):
+        if os.path.isfile(os.path.join(directory, filename)):
+            file_names.append(filename)
+    return file_names
+
+files_in_directory = get_all_files_in_directory(dataset_dir)
+
+dataset_info = []
+for file_name in files_in_directory:
+    mtx_filepath = os.path.join(dataset_dir, file_name)
+    # 从文件读3个数据，数据的类型为小端法（<）的4字节整数（i4）
+    num_row, num_col, num_nonezero = np.fromfile(
+        mtx_filepath, count=3, dtype = '<i4'
+    )
+    dataset_info.append([
+        file_name.split(".")[0], num_row, num_col, 
+         num_nonezero, num_nonezero*1.0/num_row/num_col
+    ])
+
+csv_file_path = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), 'dataset_analyse.csv'
+)
+
+with open(csv_file_path, 'w', newline='') as csv_file:
+    csv_writer = csv.writer(csv_file)
+    # 写入 CSV 文件的标题行
+    csv_writer.writerow([
+        "File Name", "Num Rows", "Num Cols", "Num Nonzero", "Sparsity"
+    ])
+    for data_row in dataset_info:
+        csv_writer.writerow(data_row)
+
+### Select dataset
+seletced_dataset_info = []
+for data_row in dataset_info:
+    if data_row[1] <= 131072 and data_row[3] < 10_000_000:
+        seletced_dataset_info.append(data_row)
+
+csv_file_path = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), 'selected_dataset_analyse.csv'
+)
+with open(csv_file_path, 'w', newline='') as csv_file:
+    csv_writer = csv.writer(csv_file)
+    # 写入 CSV 文件的标题行
+    csv_writer.writerow([
+        "File Name", "Num Rows", "Num Cols", "Num Nonzero", "Sparsity"
+    ])
+    for data_row in seletced_dataset_info:
+        csv_writer.writerow(data_row)
+
+output_filename_total = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), 'autosparse_total.txt'
+)
+output_filename_validation = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), 'autosparse_validation.txt'
+)
+output_filename_train = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), 'autosparse_total.txt'
+)
---
+++ suitsparce/get_origin_ss.py
@@ -0,0 +1,64 @@
+# Only run in linux enviroment.
+import ssgetpy
+import os, sys
+import csv
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+def download_data():
+    selected_matrix = ssgetpy.search(
+        rowbounds=(0, 131072), nzbounds=(0, 10_000_000),
+        is2d3d = False, limit = 10000
+    )
+
+    matrix_info = [] # Get all the matrix info
+    for matrix in selected_matrix:
+        matrix_info.append(
+            [
+                matrix.id,
+                matrix.group,
+                matrix.name,
+                matrix.rows,
+                matrix.cols,
+                matrix.nnz,
+                matrix.nnz * 1.0 /  matrix.rows / matrix.cols,
+                matrix.dtype,
+                matrix.is2d3d,
+                matrix.isspd,
+                matrix.psym,
+                matrix.nsym,
+                matrix.kind,
+                matrix.name + ".mat"
+            ]
+        )
+
+    format = "MAT"
+    destpath = os.path.join(current_dir, "mat_data")
+    selected_matrix.download(format = format, destpath = destpath, extract = True)
+    print("Successed Download MAT data.")
+
+    # Save info of matirx
+    csv_file_path = os.path.join(
+        current_dir, 'matrix_info.csv'
+    )
+    with open(csv_file_path, 'w', newline='') as csv_file:
+        csv_writer = csv.writer(csv_file)
+        # 写入 CSV 文件的标题行
+        csv_writer.writerow([
+            "Id", "Group", "Name", "Num Rows", "Num Cols", "Num Nonzero", 
+            "Sparsity", "DType", "Is 2D/3D", "Is SPD", "Pattern Symmetry",
+            "Numerical Symmetry", "Kind", "File Path"
+        ])
+        for data_row in matrix_info:
+            csv_writer.writerow(data_row)
+
+if __name__ == "__main__":
+    download_data()
+
+
+
+
+
+
+
+
