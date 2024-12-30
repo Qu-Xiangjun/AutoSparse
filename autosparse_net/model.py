ac4ef70971d7b7f82890d6652eab6a30d38efef6
quxiangjun <quxiangjun@mail.ustc.edu.cn>
2024-12-30 23:36:31 +0800

    Add autosparse net and tokenizer

---
+++ autosparse_net/model.py
@@ -0,0 +1,434 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import MinkowskiEngine as ME
+from typing import *
+from tokenizer import Tokenizer
+
+
+class SparseMatrixEmbed_WACO_NET(nn.Model):
+
+    def __init__(self, in_channels=1, middle_channels=32, out_feature=128, D=2):
+        self.inplanes = middle_channels
+        self.layer1 = nn.Sequential(
+            ME.MinkowskiConvolution(
+                in_channels, self.inplanes, kernel_size=5, stride=1, dimension=D
+            ),
+            ME.MinkowskiReLU(inplace=True),
+        )
+        self.layer2 = nn.Sequential(
+            # 这些卷积是不是只设置了步长比较大，但没设置Dilate 的空洞卷积，是否可以使用空洞卷积呢？
+            # 另外是否需要bias 呢？
+            ME.MinkowskiConvolution(
+                self.inplanes, self.inplanes, kernel_size=3, stride=2, dimension=D
+            ),
+            ME.MinkowskiReLU(inplace=True),
+        )
+        self.layer3 = nn.Sequential(
+            ME.MinkowskiConvolution(
+                self.inplanes, self.inplanes, kernel_size=3, stride=2, dimension=D
+            ),
+            ME.MinkowskiReLU(inplace=True),
+        )
+        self.layer4 = nn.Sequential(
+            ME.MinkowskiConvolution(
+                self.inplanes, self.inplanes, kernel_size=3, stride=2, dimension=D
+            ),
+            ME.MinkowskiReLU(inplace=True),
+        )
+        self.layer5 = nn.Sequential(
+            ME.MinkowskiConvolution(
+                self.inplanes, self.inplanes, kernel_size=3, stride=2, dimension=D
+            ),
+            ME.MinkowskiReLU(inplace=True),
+        )
+        self.layer6 = nn.Sequential(
+            ME.MinkowskiConvolution(
+                self.inplanes, self.inplanes, kernel_size=3, stride=2, dimension=D
+            ),
+            ME.MinkowskiReLU(inplace=True),
+        )
+        self.layer7 = nn.Sequential(
+            ME.MinkowskiConvolution(
+                self.inplanes, self.inplanes, kernel_size=3, stride=2, dimension=D
+            ),
+            ME.MinkowskiReLU(inplace=True),
+        )
+        self.layer8 = nn.Sequential(
+            ME.MinkowskiConvolution(
+                self.inplanes, self.inplanes, kernel_size=3, stride=2, dimension=D
+            ),
+            ME.MinkowskiReLU(inplace=True),
+        )
+        self.layer9 = nn.Sequential(
+            ME.MinkowskiConvolution(
+                self.inplanes, self.inplanes, kernel_size=3, stride=2, dimension=D
+            ),
+            ME.MinkowskiReLU(inplace=True),
+        )
+        self.layer10 = nn.Sequential(
+            ME.MinkowskiConvolution(
+                self.inplanes, self.inplanes, kernel_size=3, stride=2, dimension=D
+            ),
+            ME.MinkowskiReLU(inplace=True),
+        )
+        self.layer11 = nn.Sequential(
+            ME.MinkowskiConvolution(
+                self.inplanes, self.inplanes, kernel_size=3, stride=2, dimension=D
+            ),
+            ME.MinkowskiReLU(inplace=True),
+        )
+        self.layer12 = nn.Sequential(
+            ME.MinkowskiConvolution(
+                self.inplanes, self.inplanes, kernel_size=3, stride=2, dimension=D
+            ),
+            ME.MinkowskiReLU(inplace=True),
+        )
+        self.layer13 = nn.Sequential(
+            ME.MinkowskiConvolution(
+                self.inplanes, self.inplanes, kernel_size=3, stride=2, dimension=D
+            ),
+            ME.MinkowskiReLU(inplace=True),
+        )
+        self.layer14 = nn.Sequential(
+            ME.MinkowskiConvolution(
+                self.inplanes, self.inplanes, kernel_size=3, stride=2, dimension=D
+            ),
+            ME.MinkowskiReLU(inplace=True),
+        )
+
+        self.glob_pool = nn.Sequential(
+            ME.MinkowskiGlobalAvgPooling(), ME.MinkowskiToFeature()
+        )
+
+        self.shape_feature = nn.Sequential(
+            nn.Linear(3, 64),
+            nn.ReLU(),
+            nn.Linear(64, 32),
+        )
+
+        self.matrix_embedding = nn.Sequential(
+            nn.Linear(self.INIT_DIM * 14, 256),
+            nn.ReLU(),
+            nn.Linear(256, out_feature),
+        )
+
+    def weight_initialization(self):
+        for m in self.modules():
+            if isinstance(m, ME.MinkowskiConvolution):
+                ME.utils.kaiming_normal_(m.kernel, mode="fan_out", nonlinearity="relu")
+
+    def forward(self, x1: ME.SparseTensor):
+        """
+        Parameters
+        ----------
+        x1 : ME.SparseTensor
+            Sparse matrix.
+        x2 : torch.Tensor
+            Normalized shape info: (row_num, col_num, nnz)
+        """
+        y1 = self.layer1(x1)  # [batch, H, W] -> [batch, H, W]
+        y2 = self.layer2(y1)
+        y3 = self.layer3(y2)
+        y4 = self.layer4(y3)
+        y5 = self.layer5(y4)
+        y6 = self.layer6(y5)
+        y7 = self.layer7(y6)
+        y8 = self.layer8(y7)
+        y9 = self.layer9(y8)
+        y10 = self.layer10(y9)
+        y11 = self.layer11(y10)
+        y12 = self.layer12(y11)
+        y13 = self.layer13(y12)
+        y14 = self.layer14(y13)
+
+        y1 = self.glob_pool(y1)  # [batch, H, W] -> [batch, H, W]
+        y2 = self.glob_pool(y2)
+        y3 = self.glob_pool(y3)
+        y4 = self.glob_pool(y4)
+        y5 = self.glob_pool(y5)
+        y6 = self.glob_pool(y6)
+        y7 = self.glob_pool(y7)
+        y8 = self.glob_pool(y8)
+        y9 = self.glob_pool(y9)
+        y10 = self.glob_pool(y10)
+        y11 = self.glob_pool(y11)
+        y12 = self.glob_pool(y12)
+        y13 = self.glob_pool(y13)
+        y14 = self.glob_pool(y14)
+
+        # y = F.normalize(torch.cat((y1,y2,y3,y4,y5,y6,y7,y8,y9,y10,y11,y12,y13,y14), dim=1))
+        y = torch.cat(
+            (y1, y2, y3, y4, y5, y6, y7, y8, y9, y10, y11, y12, y13, y14), dim=-1
+        )
+
+        # 这里不类似WACO NET将shape info嵌入matrix embedding，只用其卷积部分即可
+        y = self.matrix_embedding(y)
+        # y = F.normalize(y)
+
+        return y
+
+
+class SparseMatrixEmbedNet(nn.Model):
+    """Extract sparse feature from sparse matrix data."""
+
+    def __init__(self, in_channels=1, middle_channels=128, out_feature=128, D=2):
+        """
+        Parameters
+        ----------
+        in_channels : int, optional
+            _description_, by default 1
+        middle_channels : int, optional
+            Use some middle hid channel to contain more feature info, by default 32
+        out_feature : int, optional
+            _description_, by default 1
+        D : int, optional
+            Sparse matirx dimension count, by default 2
+        """
+        nn.Module.__init__(self)
+        self.in_channels = in_channels
+        self.middle_channels = middle_channels
+        self.out_feature = out_feature
+        self.D = D
+        self.layer1 = nn.Sequential(
+            ME.MinkowskiConvolution(
+                in_channels, middle_channels, kernel_size=5, stride=1, dimension=D
+            ),
+            ME.MinkowskiReLU(inplace=True),
+        )
+
+        # 使用 HDC Block 的空洞卷积来做提取
+        self.layer2 = nn.Sequential(
+            ME.MinkowskiConvolution(
+                middle_channels,
+                middle_channels,
+                kernel_size=3,
+                stride=1,
+                dilation=1,
+                dimension=D,
+            ),
+            ME.MinkowskiReLU(inplace=True),
+            ME.MinkowskiConvolution(
+                middle_channels,
+                middle_channels,
+                kernel_size=3,
+                stride=1,
+                dilation=2,
+                dimension=D,
+            ),
+            ME.MinkowskiReLU(inplace=True),
+            ME.MinkowskiConvolution(
+                middle_channels,
+                middle_channels,
+                kernel_size=3,
+                stride=1,
+                dilation=3,
+                dimension=D,
+            ),
+            ME.MinkowskiReLU(inplace=True),
+        )
+        self.layer3 = nn.Sequential(
+            ME.MinkowskiConvolution(
+                middle_channels,
+                middle_channels,
+                kernel_size=3,
+                stride=1,
+                dilation=1,
+                dimension=D,
+            ),
+            ME.MinkowskiReLU(inplace=True),
+            ME.MinkowskiConvolution(
+                middle_channels,
+                middle_channels,
+                kernel_size=3,
+                stride=1,
+                dilation=2,
+                dimension=D,
+            ),
+            ME.MinkowskiReLU(inplace=True),
+            ME.MinkowskiConvolution(
+                middle_channels,
+                middle_channels,
+                kernel_size=3,
+                stride=1,
+                dilation=3,
+                dimension=D,
+            ),
+            ME.MinkowskiReLU(inplace=True),
+        )
+        self.layer4 = nn.Sequential(
+            ME.MinkowskiConvolution(
+                middle_channels,
+                middle_channels,
+                kernel_size=3,
+                stride=1,
+                dilation=1,
+                dimension=D,
+            ),
+            ME.MinkowskiReLU(inplace=True),
+            ME.MinkowskiConvolution(
+                middle_channels,
+                middle_channels,
+                kernel_size=3,
+                stride=1,
+                dilation=2,
+                dimension=D,
+            ),
+            ME.MinkowskiReLU(inplace=True),
+            ME.MinkowskiConvolution(
+                middle_channels,
+                middle_channels,
+                kernel_size=3,
+                stride=1,
+                dilation=3,
+                dimension=D,
+            ),
+            ME.MinkowskiReLU(inplace=True),
+        )
+        self.glob_pool = nn.Sequential(
+            ME.MinkowskiGlobalAvgPooling(), ME.MinkowskiToFeature()
+        )
+
+        # 类似 WACO NET 合并多个中间卷积结果
+        self.feature1 = nn.Sequential(
+            nn.Linear(middle_channels * 4, 512), nn.ReLU(), nn.Linear(512, out_feature)
+        )
+
+        # 只使用简单的卷积结果，寄希望于充足的channel
+        self.feature2 = nn.Sequential(
+            nn.Linear(middle_channels, 512), nn.ReLU(), nn.Linear(512, out_feature)
+        )
+
+    def weight_initialization(self):
+        for m in self.modules():
+            if isinstance(m, ME.MinkowskiConvolution):
+                ME.utils.kaiming_normal_(m.kernel, mode="fan_out", nonlinearity="relu")
+
+    def forward1(self, x: ME.SparseTensor):
+        """Same with WACO_NET using results of multi conv layer."""
+        y1 = self.layer1(x)
+        y2 = self.layer2(y1)
+        y3 = self.layer3(y2)
+        y4 = self.layer4(y3)
+
+        y1 = self.glob_pool(y1)
+        y2 = self.glob_pool(y2)
+        y3 = self.glob_pool(y3)
+        y4 = self.glob_pool(y4)
+
+        y = self.feature1(torch.cat((y1, y2, y3, y4), dim=-1))
+
+        return y
+
+    def forward2(self, x: ME.SparseTensor):
+        """Only rely on last conv layer result."""
+        y1 = self.layer1(x)
+        y2 = self.layer2(y1)
+        y3 = self.layer3(y2)
+        y4 = self.layer4(y3)
+        y4 = self.glob_pool(y4)
+        y = self.feature2(y4)
+        return y
+
+
+class AutoSparseNet(nn.Module):
+    """Cost model for AutoSparse, using conv net embedding sparse matrix,
+    and using transformer decoder to preidict program ranking.
+    """
+
+    def __init__(
+        self,
+        in_channels=1,
+        middle_channels=128,
+        embedding_size: int = 128,
+        D=2,
+        tensor_name_set: List[str] = None,
+    ):
+        """_summary_
+
+        Parameters
+        ----------
+        in_channels : int, optional
+            _description_, by default 1
+        middle_channels : int, optional
+            Use some middle hid channel to contain more feature info, by default 32
+        D : int, optional
+            Sparse matirx dimension count, by default 2
+        embedding_size : int, optional
+            _description_, by default 128
+        tensor_name_set : List[str], optional
+            All existed tensor name, by default None
+        """
+        self.embedding_size = embedding_size
+        self.conv_sparse_feature_waco = SparseMatrixEmbed_WACO_NET(
+            in_channels, middle_channels, embedding_size, D
+        )
+        self.conv_sparse_feature = SparseMatrixEmbedNet(
+            in_channels, middle_channels, embedding_size, D
+        )
+
+        self.tokenizer = Tokenizer(embedding_size, tensor_name_set)
+        
+        self.encoder = nn.Sequential(
+            nn.Linear(embedding_size, 128),
+            nn.ReLU(),
+            nn.Linear(256, 256),
+            nn.ReLU(),
+        )
+        self.attention = nn.MultiheadAttention(embed_dim = 256, num_heads = 8)
+        self.res1 = nn.Sequential(
+            nn.Linear(embedding_size, 256),
+            nn.ReLU(),
+        )
+        self.res2 = nn.Sequential(
+            nn.Linear(256, 256),
+            nn.ReLU(),
+        )
+        self.decoder = nn.Sequential(
+            nn.Linear(256, 128),
+            nn.ReLU(),
+            nn.Linear(128, 64),
+            nn.ReLU(),
+            nn.Linear(64, 1),
+        )
+
+    def embed_sparse_matirx_WACO(self, x: ME.SparseTensor):
+        return self.conv_sparse_feature_waco(x)
+
+    def embed_sparse_matirx(self, x: ME.SparseTensor):
+        return self.conv_sparse_feature(x)
+
+    def forward(self, schedules: Union[str, List[str]], sparse_tensor_info: Tuple[float], sparse_matrix: ME.SparseTensor):
+        input_seq = self.tokenizer(schedules, sparse_tensor_info)
+        embeded_sparse_feature = self.embed_sparse_matirx(sparse_matrix).reshape(1, 1, self.embedding_size)
+        embeded_sparse_feature = embeded_sparse_feature.expand(input_seq.size(0), -1, -1)
+
+        input_seq_full = torch.cat((input_seq, embeded_sparse_feature), dim = 1)
+        
+        encoder_seq = self.encoder(input_seq_full).transpose(0, 1) # batch first is False
+        output, attention_mask = self.attention(encoder_seq, encoder_seq, encoder_seq)
+        output = output + self.res1(output)
+        output = output + self.res2(output)
+        output = self.decoder(output) # shape [seq_len, batch, 1]
+
+        output = output.sum(0).squeeze()
+
+        return output
+
+
+    def forward_in_query(self, schedules: Union[str, List[str]], sparse_tensor_info: Tuple[float], sparse_matrix_embeded_feature: torch.Tensor):
+        input_seq = self.tokenizer(schedules, sparse_tensor_info)
+        embeded_sparse_feature = sparse_matrix_embeded_feature.reshape(1, 1, self.embedding_size)
+        embeded_sparse_feature = embeded_sparse_feature.expand(input_seq.size(0), -1, -1)
+
+        input_seq_full = torch.cat((input_seq, embeded_sparse_feature), dim = 1)
+        
+        encoder_seq = self.encoder(input_seq_full).transpose(0, 1) # batch first is False
+        output, attention_mask = self.attention(encoder_seq, encoder_seq, encoder_seq)
+        output = output + self.res1(output)
+        output = output + self.res2(output)
+        output = self.decoder(output) # shape [seq_len, batch, 1]
+
+        output = output.sum(0).squeeze()
+
+        return output
---
+++ autosparse_net/tokenizer.py
@@ -0,0 +1,235 @@
+import os, sys
+import random
+import torch
+import numpy as np
+import pandas
+from typing import *
+
+
+class Tokenizer:
+    """
+    Encode all schedule primitives and sparse matrix feature information into tokens.
+    """
+
+    PRIMITIVES = [
+        "fsplit",
+        "freorder",
+        "fmode",
+        "lsplit",
+        "lreorder",
+        "lparallel",
+        "lunroll",
+        "lvector",
+        "openmp_parameter",
+        "sparse_info",
+    ]
+
+    def __init__(self, embedding_size: int = 128, tensor_name_set: List[str] = None):
+        """_summary_
+
+        Parameters
+        ----------
+        embedding_size : int, optional
+            _description_, by default 128
+        tensor_name_set : List[str], optional
+            All existed tensor name, by default None
+        """
+        self.embedding_size = embedding_size
+        self.primitives_one_hot = {}
+        for idx, key in enumerate(self.PRIMITIVES):
+            self.primitives_one_hot[key] = [0 for i in range(len(self.PRIMITIVES))]
+            self.primitives_one_hot[key][idx] = 1
+        if tensor_name_set == None:
+            tensor_name_set = [i for i in "ABCDEFGHIJKLMNOPQRST"]
+        self.character_mapping = ["<START>", "", " ", "None"] + tensor_name_set
+        for c in "ijklmnopqrstuvwxyzabcdefgh":
+            self.character_mapping.append(c)
+            for id0 in "0123456789":
+                self.character_mapping.append(c + id0)
+                for id1 in "0123":
+                    self.character_mapping.append(c + id0 + id1)
+
+    @staticmethod
+    def ScheduleParse(schedule: str) -> List[str]:
+        parsed_sch = []
+        sch = schedule.split(" ")
+        fsplit_count = int(sch[0])
+        now_idx = 1
+        for idx in range(fsplit_count):
+            fsplit_vec = ["fsplit"]
+            fsplit_vec.append(sch[now_idx])
+            splited_cnt = int(sch[now_idx + 1])
+            fsplit_vec.extend(sch[now_idx + 2 : now_idx + 2 + splited_cnt * 2])
+            parsed_sch.append(fsplit_vec)  # ["fsplit" i i0 256 i1 16]
+            now_idx += 2 + splited_cnt * 2
+        sparse_tensor_count = int(sch[now_idx])
+        now_idx += 1
+        for idx in range(sparse_tensor_count):
+            freorder_vec = ["freorder"]
+            freorder_vec.append(sch[now_idx])
+            axes_size = int(sch[now_idx + 1])
+            now_idx += 2
+            freorder_vec.extend(sch[now_idx : now_idx + axes_size])
+            parsed_sch.append(freorder_vec)  # ["freorder" A i0 i1 k0 k1]
+            now_idx += axes_size
+        for idx in range(sparse_tensor_count):
+            fmode_vec = ["fmode"]
+            fmode_vec.append(sch[now_idx])
+            axes_size = int(sch[now_idx + 1])
+            now_idx += 2
+            fmode_vec.extend(sch[now_idx : now_idx + axes_size * 2])
+            parsed_sch.append(fmode_vec)  # ["fmode" A i0 1 i1 1 k0 1 k1 1]
+            now_idx += axes_size * 2
+        lsplit_count = int(sch[now_idx])
+        now_idx += 1
+        for idx in range(lsplit_count):
+            lsplit_vec = ["lsplit"]
+            lsplit_vec.append(sch[now_idx])
+            splited_cnt = int(sch[now_idx + 1])
+            lsplit_vec.extend(sch[now_idx + 2 : now_idx + 2 + splited_cnt * 2])
+            parsed_sch.append(lsplit_vec)  # ["lsplit" i i0 256 i1 16]
+            now_idx += 2 + splited_cnt * 2
+        lreorder_vars_count = int(sch[now_idx])
+        now_idx += 1
+        # ["lreorder" j0 i0 i1 k0 k1 j1]
+        lreorder_vec = ["lreorder"] + sch[now_idx : now_idx + lreorder_vars_count]
+        now_idx += lreorder_vars_count
+        lparallel_vec = ["lparallel", sch[now_idx]]  # ["lparallel" j0]
+        lvector_vec = ["lvector", sch[now_idx + 1]]  # ["lvector" None]
+        lunroll_vec = ["lunroll"] + sch[now_idx + 2 : now_idx + 4]  # ["lunroll" k1]
+        parsed_sch.extend([lreorder_vec, lparallel_vec, lvector_vec, lunroll_vec])
+        now_idx += 4
+        openmp_parameter_vec = ["openmp_parameter"] + sch[now_idx : now_idx + 2]
+        parsed_sch.append(openmp_parameter_vec)  # ["openmp_parameter" 128 2]
+
+        return parsed_sch
+
+    def BatchPadSequences(
+        self, sequences: List[List], max_length: int = None, padding_value=0
+    ):
+        """Padding of 0 is added to make a batch for the input data sequence.
+
+        Parameters
+        ----------
+        sequences : List[List]
+            Input sequence for every data, which shape is [batch_size, different seq len, embedding size]
+        max_length : int, optional
+            _description_, by default None
+        padding_value : int, optional
+            _description_, by default 0
+
+        Returns
+        -------
+        np.array
+            _description_
+        """
+        if max_length is None:
+            max_length = max(len(seq) for seq in sequences)
+
+        padded_sequences = []
+        for seq in sequences:
+            if len(seq) < max_length:
+                padded_seq = list(seq) + [[padding_value] * self.embedding_size] * (
+                    max_length - len(seq)
+                )
+            else:
+                padded_seq = seq[:max_length]
+            padded_sequences.append(padded_seq)
+
+        return np.array(padded_sequences)
+
+    def EmbedHelper(self, schedule: List[str]) -> List[int]:
+        embedded_tokens = []
+        for primitive_vec in schedule:
+            primitive_name = primitive_vec[0]
+            token = self.primitives_one_hot[primitive_name].copy()
+            if primitive_name == self.PRIMITIVES[0]:  # ["fsplit" i i0 256 i1 16]
+                token.append(self.character_mapping.index(primitive_vec[1]))
+                for i in range(2, len(primitive_vec), 2):
+                    token.append(self.character_mapping.index(primitive_vec[i]))
+                    token.append(int(primitive_vec[i + 1]))
+            elif primitive_name == self.PRIMITIVES[1]:  # ["freorder" A i0 i1 k0 k1]
+                for i in range(1, len(primitive_vec)):
+                    token.append(self.character_mapping.index(primitive_vec[i]))
+            elif (
+                primitive_name == self.PRIMITIVES[2]
+            ):  # ["fmode" A i0 1 i1 1 k0 1 k1 1]
+                token.append(self.character_mapping.index(primitive_vec[1]))
+                for i in range(2, len(primitive_vec), 2):
+                    token.append(self.character_mapping.index(primitive_vec[i]))
+                    token.append(int(primitive_vec[i + 1]))
+            elif primitive_name == self.PRIMITIVES[3]:  # ["lsplit" i i0 256 i1 16]
+                token.append(self.character_mapping.index(primitive_vec[1]))
+                for i in range(2, len(primitive_vec), 2):
+                    token.append(self.character_mapping.index(primitive_vec[i]))
+                    token.append(int(primitive_vec[i + 1]))
+            elif primitive_name == self.PRIMITIVES[4]:  # ["lreorder" j0 i0 i1 k0 k1 j1]
+                for i in range(1, len(primitive_vec)):
+                    token.append(self.character_mapping.index(primitive_vec[i]))
+            elif primitive_name == self.PRIMITIVES[5]:  # ["lparallel" j0]
+                token.append(self.character_mapping.index(primitive_vec[1]))
+            elif primitive_name == self.PRIMITIVES[6]:  # ["lunroll" k1, 4]
+                token.append(self.character_mapping.index(primitive_vec[1]))
+                token.append(int(primitive_vec[2]))
+            elif primitive_name == self.PRIMITIVES[7]:  # ["lvector" None]
+                token.append(self.character_mapping.index(primitive_vec[1]))
+            elif primitive_name == self.PRIMITIVES[8]:  # ["openmp_parameter" 128 2]
+                token.append(int(primitive_vec[1]))
+                token.append(int(primitive_vec[2]))
+            elif (
+                primitive_name == self.PRIMITIVES[9]
+            ):  # ["sparse_info" 0.123 0.35 0.13 0.1284]
+                for i in range(1, len(primitive_vec)):
+                    assert isinstance(primitive_vec[i], float)
+                    token.append(primitive_vec[i])
+            else:
+                assert False, f"{primitive_vec[0]} have not in PRIMITIVES"
+            assert (
+                len(token) <= self.embedding_size
+            ), f"embedded token size {len(token)} more than {self.embedding_size}."
+            token.extend([0 for i in range(self.embedding_size - len(token))])
+            embedded_tokens.append(token)
+        return embedded_tokens
+
+    def __call__(
+        self, schedules: Union[str, List[str]], sparse_tensor_info: Tuple[float]
+    ) -> torch.Tensor:
+        """
+        Parameters
+        ----------
+        schedules : Union[str, List[str]]
+            _description_
+        sparse_tensor_info : Tuple[float]
+            Constructed in [row, col, nonezeros, sparsity]. Notice they usually be normalized.
+
+        Returns
+        -------
+        torch.Tensor
+            Return the batched token sequences.
+        """
+        # sparse_tensor_info = sparse_tensor_shape: Tuple[int], nonezeros, sparsity
+        if isinstance(schedules, str):
+            schedules = [schedules]
+        elif not isinstance(schedules, List):
+            assert False, "Schedules only support type with str and List[str]."
+
+        sequences = []
+        for sch in schedules:
+            assert isinstance(schedules[0], str), "Schedule must contain str type."
+            sch_vecs = self.ScheduleParse(sch)
+            sch_vecs.append(["sparse_info", *sparse_tensor_info])
+            embedded_tokens = self.EmbedHelper(sch_vecs)
+            sequences.append(embedded_tokens)
+
+        return torch.from_numpy(self.BatchPadSequences(sequences))
+
+
+if __name__ == "__main__":
+    schedules = [
+        "2 i 2 i0 256 i1 16 k 2 k0 4096 k1 16 1 A 4 i0 i1 k0 k1 A 4 i0 1 i1 1 k0 1 k1 1 1 j 2 j0 2 j1 64 6 j0 i0 i1 k0 k1 j1 j0 j1 None 0 128 512 445.855559",
+        "2 i 2 i0 32 i1 64 k 2 k0 512 k1 64 1 A 4 k0 i0 k1 i1 A 4 i0 1 i1 0 k0 2 k1 1 1 j 2 j0 8 j1 16 6 j0 j1 k0 i0 k1 i1 j0 i1 j1 16 128 2 309.067448",
+        "2 i 2 i0 4096 i1 16 j 2 j0 1024 j1 8 1 A 4 i0 j0 i1 j1 A 4 i0 2 i1 1 j0 4 j1 1 0 4 i0 j0 i1 j1 None None None 0 128 2 5.13304",
+    ]
+    tokenizer = Tokenizer()
+    embedded_tokens = tokenizer(schedules, [0.123, 0.35, 0.13, 0.1284])
+    print(embedded_tokens)
